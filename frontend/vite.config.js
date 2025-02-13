312388f643dfd35cb887365a8f246a90b35f3b5c
Praveen Kumar <praveentatikonda62@gmail.com>
2025-02-13 10:02:30 +0530

    added static.json for render-4 & deleted vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ Ensures correct asset loading
+  base: "/", //  Ensures correct asset loading
   server: {
     proxy: {
       "/api": {
