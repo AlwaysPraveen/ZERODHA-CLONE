d1313812b39a8e7436e056f6242272b91fea7180
Praveen Kumar <praveentatikonda62@gmail.com>
2025-02-14 22:51:35 +0530

    Adjusted Protected Route to resolve redirecting issue

--- dashboard/src/ProtectedRoute.jsx
+++ dashboard/src/ProtectedRoute.jsx
@@ -1,12 +1,19 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "./Auth";
+import { useEffect } from "react";
+import { useAuth } from "./AuthContext";
 
 const ProtectedRoute = ({ children }) => {
     const { user, loading } = useAuth();
 
     if (loading) return <p>Loading...</p>; // Prevent unauthorized flashes
 
-    return user ? children : window.location.href = "https://zerodha-clone-2-dyji.onrender.com/signup";
+    if (!user) {
+        useEffect(() => {
+            window.location.href = "https://zerodha-clone-2-dyji.onrender.com/signup"; // Redirect to separate frontend (Login page is here dude)
+        }, []);
+        return null; // Prevent rendering anything before redirect
+    }
+
+    return children;
 };
 
 export default ProtectedRoute;
